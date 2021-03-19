9c083c36e4c0c89b65f032e0ddefb16be5217a1c
Replit user <replituser@example.com>
2021-03-19 19:47:02 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -123,7 +123,7 @@ void sumaV2(int a1, int a2, int b1, int b2, Vector *c){
   asignarValor(a, 0, a1);
   asignarValor(a, 1, a2);
   asignarValor(b, 0, b1);
-  asignarValor(b, 1, b1);
+  asignarValor(b, 1, b2);
   //c->datos = (int*)malloc(c->capacidad*sizeof(int));
   sumaV(a,b,c);
   /*
